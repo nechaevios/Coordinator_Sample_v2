4efc28011089368148816a517f85482afd8255a1
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-03-07 21:41:46 +0300

    Initial Commit

---
+++ Coordinator_Sample_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinator_Sample_2
+//
+//  Created by Nechaev Sergey  on 07.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
