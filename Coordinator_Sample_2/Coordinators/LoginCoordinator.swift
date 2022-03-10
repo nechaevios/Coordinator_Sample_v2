637e216114c29567824623f97a73ce0056c6f411
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-03-10 18:12:07 +0300

    LoginCoordinator and MainCoordinator added

---
+++ Coordinator_Sample_2/Coordinators/LoginCoordinator.swift
@@ -0,0 +1,58 @@
+//
+//  LoginCoordinator.swift
+//  Coordinator_Sample_2
+//
+//  Created by Nechaev Sergey  on 10.03.2022.
+//
+
+import UIKit
+
+protocol LoginCoordinatorProtocol: Coordinator {
+    func showLogin()
+}
+
+class LoginCoordinator : LoginCoordinatorProtocol {
+
+    weak var finishDelegate: CoordinatorFinishDelegate?
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    var type: CoordinatorType = .login
+
+    required init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    deinit {
+        print("LoginCoordinator deallocated")
+    }
+
+    func start() {
+        showLogin()
+    }
+
+    func showLogin() {
+        let loginVC = LogInViewController()
+        loginVC.viewModel = LogInViewModel()
+
+        loginVC.navigationItem.title = "LogIn"
+//        navigationController.navigationBar.prefersLargeTitles = true
+
+        loginVC.viewModel.didSendEventClosure = { [weak self] eventType in
+            switch eventType {
+            case .forgotPassword:
+                self?.showResetPassword()
+            case .main:
+                self?.type = .main
+                self?.finish()
+            }
+        }
+
+        navigationController.pushViewController(loginVC, animated: false)
+    }
+
+    private func showResetPassword() {
+        let resetVC = PasswordResetViewController()
+        resetVC.navigationItem.title = "Reset"
+        navigationController.pushViewController(resetVC, animated: true)
+    }
+}
---
+++ Coordinator_Sample_2/Coordinators/MainCoordinator.swift
@@ -0,0 +1,52 @@
+//
+//  MainCoordinator.swift
+//  Coordinator_Sample_2
+//
+//  Created by Nechaev Sergey  on 10.03.2022.
+//
+
+import UIKit
+
+protocol MainCoordinatorProtocol: Coordinator {
+    func showMain()
+}
+
+class MainCoordinator: MainCoordinatorProtocol {
+
+    weak var finishDelegate: CoordinatorFinishDelegate?
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    var type: CoordinatorType = .main
+
+    func start() {
+        showMain()
+    }
+
+    func showMain() {
+        let mainVC = MainViewController()
+        mainVC.viewModel = MainViewModel()
+
+        mainVC.navigationItem.title = "Main"
+//        navigationController.navigationBar.prefersLargeTitles = true
+
+        mainVC.viewModel?.didSendEventClosure = { [weak self] eventType in
+            switch eventType {
+            case .logOut:
+                self?.type = .login
+                self?.finish()
+            }
+
+        }
+
+        navigationController.pushViewController(mainVC, animated: false)
+    }
+
+    required init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    deinit {
+        print("MainCoordinator deallocated")
+    }
+
+}
